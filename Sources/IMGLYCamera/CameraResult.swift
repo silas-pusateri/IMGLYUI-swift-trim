e91bec6888525a01cfc470bc8aa8220dd153e33d
silas-pusateri <52931481+silas-pusateri@users.noreply.github.com>
2025-02-12 11:46:57 -0600

    Delete Sources/IMGLYCamera directory

--- Sources/IMGLYCamera/CameraResult.swift
+++
@@ -1,7 +0,0 @@
-/// Wraps the result of the camera.
-public enum CameraResult: Sendable {
-  /// An array of recordings.
-  case recording([Recording])
-  /// The video that was reacted to and an array of reaction recordings.
-  case reaction(video: Recording, reaction: [Recording])
-}
--- Sources/IMGLYCamera/Capture/CameraStreamUpdate.swift
+++
@@ -1,8 +0,0 @@
-import AVFoundation
-
-/// The continous camera stream provides frames for either camera and finished recordings.
-enum CaptureStreamUpdate: @unchecked Sendable {
-  case output1Frame(CVImageBuffer)
-  case output2Frame(CVImageBuffer)
-  case recording(Recording)
-}
